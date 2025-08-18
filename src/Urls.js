e8d7d09a4e50298b78870c1e6f362b79e311f8a6
ishashenoy <ishashenoy007@gmail.com>
2025-08-18 12:42:49 -0400

    Changed baseUrl

--- src/Urls.js
+++ src/Urls.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://mymediatracker.onrender.com';
-export const baseUrl = 'http://localhost:3001';
+export const baseUrl = 'https://mymediatracker.onrender.com';
+// export const baseUrl = 'http://localhost:3001';
